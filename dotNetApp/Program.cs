fe5316876ec1eb29448e3896be8578142faa95a9
Andrew Chasin <andrewchasin@google.com>
2023-03-06 00:15:58 -0500

    change to new dotnet

--- dotNetApp/Program.cs
+++ dotNetApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = $"http://0.0.0.0:{port}";
+var target = Environment.GetEnvironmentVariable("TARGET") ?? "World";
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"Hello {target}!");
+
+app.Run(url);
